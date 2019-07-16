833f3f8749b9b8ae4e61ea00591402b877fd76ad
cmancode <ing.mbcarlosa@gmail.com>
2019-07-15 22:54:31 -0500

    _id.vue and blog files ok

---
+++ pages/blog/_id.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <h1>Elemento de busqueda con id: {{$route.params.id}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <ul>
+            <li>Articulo #1</li>
+            <li>Articulo #2</li>
+            <li>Articulo #3</li>
+            <li>Articulo #4</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+.container{
+    ul{
+        list-style: none;
+        text-align: center;
+        li{
+            display: inline-block;
+            padding: 10px;
+        }
+    }
+}
+</style>
